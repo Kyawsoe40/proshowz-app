f14f3d2f031d5cd2a8ebb0f600e3814cf99f1ed2
unknown <kyawsoehmh@gmail.com>
2023-11-14 07:55:47 +0630

    updated=api

--- src/components/api/index.js
+++ src/components/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const api_key='f8ba39ee36adc2edf251aca6d8956135';
+export const api_key='a6fd13d8fb407e7b1c512ddd51e6ffb5';
 
 export const api=axios.create({
     baseURL : 'https://api.themoviedb.org/3'
